22c658ce1d518267408cfb15870d3c1394c5cfbd
Jose Culla <joseculla@outlook.com>
2019-03-04 22:52:41 +0100

    implementada funcion para crear un indice a partir de una lista de frases

--- P3/SAR_p3_monkey_indexer.py
+++ P3/SAR_p3_monkey_indexer.py
@@ -2,21 +2,38 @@
 
 import sys
 
+def crear_indice(frases):
+    index = {}
+    for frase in frases:
+        for word1, word2 in zip(frase.split()[:-1], frase.split()[1:]):
+            if (index.get(word1)):
+                index[word1][0] += 1
+                index[word1][1][word2] = index[word1][1].get(word2, 0) + 1
+            else:
+                index[word1] = [1, {word2 : 1}]
+    return index
+        
+
+def obtener_frases(texto):
+    return 1
+
+def formatear_indice(index):
+    return 1
+
+def guardar_indice(filename):
+    return None
+
+def syntax_error():
+    print("debe introducir como argumento el nombre de dos ficheros")
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         syntax_error()
     else:
         input = open(sys.argv[1], "r")
         texto = "".join(input.readlines())
-        frases = obtener_frases()
-        crear_indice(frases)
-
-def crear_indice(frases):
-    for frase in frases:
-        
-
-def obtener_frases(texto):
-
-
-def syntax_error():
-    print("debe introducir como argumento el nombre de dos ficheros")
+        input.close()
+        frases = obtener_frases(texto)
+        index = crear_indice(frases)
+        index = formatear_indice(index)
+        guardar_indice(sys.argv[2])
