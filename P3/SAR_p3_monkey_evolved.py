267769b22106f2943583d84a6969acd91423d9ac
JOSÉ ANTONIO CULLA DE MOYA <joculde@upv.edu.es>
2019-03-06 10:41:52 +0100

    added console parameters management and index loading

--- P3/SAR_p3_monkey_evolved.py
+++ P3/SAR_p3_monkey_evolved.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
 import sys
+import pickle
 
+def syntax_error():
+    print("you must provide an index file")
 
-
+def load_index(filename):
+    with open(filename, "rb") as fh:
+        index = pickle.load(fh)
+    return index
 
 if __name__ == "__main__":
-    
+    if (len(sys.argv) < 2):
+        syntax_error()
+    else:
+        index = load_index(sys.argv[1])
