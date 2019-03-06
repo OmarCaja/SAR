2535d0af41608297d3ab5cafa12a307fa99d5f54
JOSÉ ANTONIO CULLA DE MOYA <joculde@upv.edu.es>
2019-03-06 11:17:15 +0100

    minkey evolved implementation completed

--- P3/SAR_p3_monkey_evolved.py
+++ P3/SAR_p3_monkey_evolved.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+import random
 
 def syntax_error():
     print("you must provide an index file")
@@ -11,8 +12,29 @@ def load_index(filename):
         index = pickle.load(fh)
     return index
 
+def generate_sentence(index):
+    word = next_word(index["$"][1])
+    sentence = "$ " + word
+    while (word != "$" and len(sentence.split()) < 25):
+        word = next_word(index[word][1])
+        sentence = sentence + " " + word
+    return sentence
+        
+def next_word(word_occurence_list):
+    choice_list = generate_choice_list(word_occurence_list)
+    return random.choice(choice_list)[0]
+
+def generate_choice_list(word_occurence_list):
+    choice_list = []
+    for number, word in word_occurence_list:
+        choice_list.append(((word + " ") * number).split())
+    return choice_list
+
+
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
         syntax_error()
     else:
         index = load_index(sys.argv[1])
+        for i in range(0, 10):
+            print(generate_sentence(index))
