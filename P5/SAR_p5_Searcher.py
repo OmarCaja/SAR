31637202f3b938dc6da248adf58c4343b6ca5f27
CULLA DE MOYA <joculde@EVIRL-014-OK.dsic.cloud>
2019-03-08 17:59:49 +0100

    added command interface on search

--- P5/SAR_p5_Searcher.py
+++ P5/SAR_p5_Searcher.py
@@ -2,16 +2,28 @@
 
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
+import argparse
 
 if __name__ == "__main__":
-    ix = open_dir("index_dir")
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("index", help="index file")
+    parser.add_argument("-q=", help="query to search")
+    parser.add_argument("--extend", action="store_true", help="indicates if you want all the information")
+
+    args = parser.parse_args()
+    
+    ix = open_dir(args.index)
     with ix.searcher() as searcher:
-        while True:
-            text = input("Dime:")
-            if len(text) == 0:
-                break
-            query = QueryParser("content", ix.schema).parse(text)
-            results = searcher.search(query)
-            for r in results:
-                print(r)
+        if args.q:
+            hola = 1
+        else:
+            while True:
+                text = input("Dime:")
+                if len(text) == 0:
+                    break
+                query = QueryParser("content", ix.schema).parse(text)
+                results = searcher.search(query)
+                for r in results:
+                    print(r)
 
