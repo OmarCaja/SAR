5cdf1d3f773e7018e1ab6614e2039078e5194fe2
CULLA DE MOYA <joculde@EVIRL-014-OK.dsic.cloud>
2019-03-08 18:20:40 +0100

    created basic functions on indexer

--- P5/SAR_p5_Indexer.py
+++ P5/SAR_p5_Indexer.py
@@ -5,6 +5,21 @@ from whoosh.index import create_in
 from whoosh.fields import Schema, ID, TEXT
 import argparse
 
+def add_to_index(file, index):
+    writer = index.writer()
+    '''index.add_document(title)'''
+    writer.commit()
+
+def get_file(docs_dir):
+    for dirname, _, files in os.walk(docs_dir):
+        for filename in files:
+            yield os.path.join(dirname, filename)
+
+def get_file_info(filename):
+    with open(filename, "r") as fh:
+        return json.load(fh)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -15,17 +30,11 @@ if __name__ == "__main__":
 
     schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
     idir = args.index_directory
-    
+
     if not os.path.exists(idir):
         os.mkdir(idir)
+
     ix = create_in(idir, schema)
 
-    writer = ix.writer()
-    writer.add_document(title="First document", path="/a",
-                    content="Este es el texto del primer documento_")
-    writer.add_document(title="Second document", path="/b",
-                    content="Este es el TEXTO de nuestro segundo documento")
-    writer.commit()
-
-
-
+    for filename in get_file(args.doc_directory):
+        add_to_index(get_file_info(filename), ix)
