25dfb2a07b4241869073399c77f388faab9b66df
Jose Culla <joseculla@outlook.com>
2019-03-09 09:55:31 +0100

    modified argparse usage message

--- P5/SAR_p5_Searcher.py
+++ P5/SAR_p5_Searcher.py
@@ -4,15 +4,18 @@ from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
 import argparse
 
+def syntax():
+    return "python SAR_p5_Searcher.py index_directory [-q=query] [--extend]"
+
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(usage=syntax())
     parser.add_argument("index", help="index file")
-    parser.add_argument("-q=", help="query to search")
+    parser.add_argument("-q", help="query to search")
     parser.add_argument("--extend", action="store_true", help="indicates if you want all the information")
 
     args = parser.parse_args()
-    
+
     ix = open_dir(args.index)
     with ix.searcher() as searcher:
         if args.q:
